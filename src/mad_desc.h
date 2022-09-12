694186d68567a47eb839c0ec8015efbbe46e59d3
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2022-09-12 10:37:24 +0200

    gtpsa: c function descriptions compatible with  c++
    required as I can not get the c++ compiler accept variable arrays
    described as (size_t n, type_t a[n]) in the function descrption ...
    
    Reverting to standard buffers ...

--- src/mad_desc.h
+++ src/mad_desc.h
@@ -63,7 +63,7 @@ void  mad_desc_del    (const desc_t *d);
 
 // -- introspection
 int   mad_desc_getnv  (const desc_t *d, ord_t *mo_, int *np_, ord_t *po_); // return nv
-ord_t mad_desc_getno  (const desc_t *d, int nn, ord_t no_[/*nn*/]); // return mo
+ord_t mad_desc_getno  (const desc_t *d, int nn, ord_t *no_); // return mo
 ord_t mad_desc_maxord (const desc_t *d); // return mo
 ssz_t mad_desc_maxlen (const desc_t *d); // ordlen(mo) == maxlen
 ssz_t mad_desc_ordlen (const desc_t *d, ord_t mo);
@@ -72,15 +72,15 @@ void  mad_desc_info   (const desc_t *d, FILE *fp_);
 void  mad_desc_info_s (const desc_t *d, ssz_t n, char buf [/*n*/]);
 
 // -- indexes / monomials
-log_t mad_desc_isvalids  (const desc_t *d, ssz_t n,       str_t s    );           // string
-log_t mad_desc_isvalidm  (const desc_t *d, ssz_t n, const ord_t m [/*n*/]);	  // mono
-log_t mad_desc_isvalidsm (const desc_t *d, ssz_t n, const idx_t m [/*n*/]);	  // sparse mono
-idx_t mad_desc_idxs      (const desc_t *d, ssz_t n,       str_t s    );		  // string
-idx_t mad_desc_idxm      (const desc_t *d, ssz_t n, const ord_t m [/*n*/]);	  // mono
-idx_t mad_desc_idxsm     (const desc_t *d, ssz_t n, const idx_t m [/*n*/]);	  // sparse mono
-idx_t mad_desc_nxtbyvar  (const desc_t *d, ssz_t n,       ord_t m [/*n*/]);
-idx_t mad_desc_nxtbyord  (const desc_t *d, ssz_t n,       ord_t m [/*n*/]);
-ord_t mad_desc_mono      (const desc_t *d, ssz_t n,       ord_t m_[/*n*/], idx_t i);
+log_t mad_desc_isvalids  (const desc_t *d, ssz_t n,       str_t s    );
+log_t mad_desc_isvalidm  (const desc_t *d, ssz_t n, const ord_t *m);
+log_t mad_desc_isvalidsm (const desc_t *d, ssz_t n, const idx_t *m);
+idx_t mad_desc_idxs      (const desc_t *d, ssz_t n,       str_t s    );
+idx_t mad_desc_idxm      (const desc_t *d, ssz_t n, const ord_t *m);
+idx_t mad_desc_idxsm     (const desc_t *d, ssz_t n, const idx_t *m);
+idx_t mad_desc_nxtbyvar  (const desc_t *d, ssz_t n,       ord_t *m);
+idx_t mad_desc_nxtbyord  (const desc_t *d, ssz_t n,       ord_t *m);
+ord_t mad_desc_mono      (const desc_t *d, ssz_t n,       ord_t *m_, idx_t i);
 
 // global cleanup (warning: no GTSPA must still be in use!)
 void  mad_desc_cleanup(void);
